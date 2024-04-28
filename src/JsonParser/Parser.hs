a8b1a794204f7ff2c7a0aa69c8e866381bcd95f8
Hadrian Hughes <hadrianhughes@gmail.com>
2024-04-28 13:04:15 +0100

    Implement Functor class for Parser type

--- src/JsonParser/Parser.hs
+++ src/JsonParser/Parser.hs
@@ -7,3 +7,9 @@ runParser (Parser p) input =
   case p input of
     Nothing     -> Nothing
     Just (x, _) -> Just x
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ \input ->
+    case p input of
+      Nothing          -> Nothing
+      Just (x, input') -> Just (f x, input')
